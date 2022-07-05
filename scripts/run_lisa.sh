2a3e27c02bbb2b3e36c15f342374d2938070bd79
David Ruhe <david.ruhe@gmail.com>
2022-07-05 12:04:58 -0400

    bugfix

--- scripts/run_lisa.sh
+++ scripts/run_lisa.sh
@@ -4,7 +4,7 @@ for fold in {0..4}; do
     for seed in {0..4}; do
         echo "Seed: $seed, Fold: $fold"
         python_cmd="WANDB_ENABLED=TRUE bash main.sh configs/m1_planar.yml --train --seed=$seed --dataset.fold=$fold --trainer.earlystopping.patience=128"
-        launch_cmd="salloc -p shared -t 24:00:00 --wrap '$python_cmd'"
+        launch_cmd="sbatch -p shared -t 24:00:00 --wrap '$python_cmd'"
         echo $launch_cmd
         eval $launch_cmd
     done
