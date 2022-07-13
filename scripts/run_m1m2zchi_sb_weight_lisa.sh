15a691165a9d8508cb0cfbb4ed7802b7d7e92429
David Ruhe <david.ruhe@gmail.com>
2022-07-13 16:38:27 -0400

    add script

---
+++ scripts/run_m1m2zchi_sb_weight_lisa.sh
@@ -0,0 +1,12 @@
+. ./activate.sh
+
+experiment_name="m1m2zchi_sb_weight"
+for sb_weight in 0 1 0.5 0.1 0.00001; do
+    python_command="WANDB_ENABLED=TRUE sh main.sh configs/m1m2zchi_sb_planar.yml --train \
+        --trainer.min_epochs=128 \
+        --model.sb_weight=$sb_weight \
+        --experiment.name=$experiment_name"_"$sb_weight \
+        --experiment.group=$experiment_name"
+    launch_cmd="sbatch -p shared -c 8 -t 24:00:00 --wrap '$python_cmd'"
+    echo $launch_cmd
+done
