28de25de3f7c563e88f64aea427fc432e5170c56
David Ruhe <david.ruhe@gmail.com>
2022-07-15 15:13:07 -0400

    cleanup for cuda

--- scripts/run_5x5.sh
+++
@@ -1,8 +0,0 @@
-. ./activate.sh
-
-for fold in {0..4}; do
-    for seed in {0..4}; do
-        echo "Seed: $seed, Fold: $fold"
-        WANDB_ENABLED=TRUE bash main.sh configs/m1_planar.yml --train --seed=$seed --dataset.fold=$fold --trainer.earlystopping.patience=128
-    done
-done
